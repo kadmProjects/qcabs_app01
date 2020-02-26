d8287c8a5bec513d83000dc3b64597a627773442
kadmProjects <40564817+kadmProjects@users.noreply.github.com>
2020-02-26 11:50:35 +0530

    Added authentication module

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .js('resources/js/login.js', 'public/js/login.js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/login.scss', 'public/css/login.css');
